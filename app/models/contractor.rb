4f29931643d92cc791ae9d398542761d4261e609
preechan <preethi.chandrasekhar@gmail.com>
2014-12-05 14:51:44 -0800

    env setup for logo

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -1,6 +1,11 @@
 class Contractor < ActiveRecord::Base
+	if Rails.end.development?
+		has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/small35.png"
+	else
 	 has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/small35.png",
 	  :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    :path => ":style/:id/:filename"
+	end
   validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
 end
